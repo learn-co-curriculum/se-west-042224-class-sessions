13080e0799f0ac51322d90f69af892145950ad0a
betalantz <betalantz@gmail.com>
2024-06-28 09:42:56 -0700

    completes basic api

--- 12-p4-restful-kickstartr/server/app.py
+++ 12-p4-restful-kickstartr/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request
+from flask import Flask, abort, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import Project, db
@@ -6,6 +6,7 @@ from models import Project, db
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 # Note: `app.json.compact = False` Configures JSON responses to print on indented lines
 app.json.compact = False
 
@@ -16,7 +17,49 @@ api = Api(app)
 
 
 class Projects(Resource):
-    pass
+    def get(self):
+        try:
+            proj_list = [proj.to_dict() for proj in Project.query.all()]
+        except:
+            abort(500)
+        return make_response(proj_list, 200)
+
+    def post(self):
+        req_json = request.get_json()
+        new_proj = Project(**req_json)
+        db.session.add(new_proj)
+        db.session.commit()
+        return make_response(new_proj.to_dict(), 201)
+
+
+class ProjectByID(Resource):
+    def get(self, id):
+        proj = Project.query.filter(Project.id == id).first()
+        if not proj:
+            abort(404)
+        return make_response(proj.to_dict(), 200)
+
+    def patch(self, id):
+        proj = Project.query.filter(Project.id == id).first()
+        if not proj:
+            abort(404)
+        for attr, value in request.get_json().items():
+            setattr(proj, attr, value)
+        db.session.add(proj)
+        db.session.commit()
+        return make_response(proj.to_dict(), 200)
+
+    def delete(self, id):
+        proj = Project.query.filter(Project.id == id).first()
+        if not proj:
+            abort(404)
+        db.session.delete(proj)
+        db.session.commit()
+        return make_response("", 204)
+
+
+api.add_resource(Projects, "/projects")
+api.add_resource(ProjectByID, "/projects/<int:id>")
 
 
 if __name__ == "__main__":
--- 12-p4-restful-kickstartr/server/seed.py
+++ 12-p4-restful-kickstartr/server/seed.py
@@ -1,4 +1,7 @@
+from random import randint
+
 from app import app
+from faker import Faker
 from models import Project, db
 
 with app.app_context():
@@ -8,7 +11,17 @@ with app.app_context():
 
     print("Seeding projects...")
 
+    fake = Faker()
+
     projects = []
 
     for _ in range(10):
-        pass
+        proj = Project(
+            owner=fake.name(),
+            title=fake.bs().title(),
+            goal_amount=randint(10000, 500000),
+            amount_raised=randint(500, 9999),
+        )
+        projects.append(proj)
+    db.session.add_all(projects)
+    db.session.commit()
